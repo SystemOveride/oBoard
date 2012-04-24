4dfeb2791a22b3a8459117a3447dbbc6e8b31fbd
SystemOveride <system_overide@live.i>
2012-04-24 15:43:17 +0200

    Configuration File

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Database Configuration File
+ */
+
+
+/* Database Configuration */
+
+$date = array(
+	"db_host" => "localhost",
+	"db_user" => "root",
+	"db_name" => "oBoard",
+	"db_password" => "testone"
+);
+
+?>
