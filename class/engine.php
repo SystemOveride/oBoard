252f35f3156d71ac8ef8f349353c2f1e39359503
SystemOveride <system_overide@live.i>
2012-05-05 13:26:39 +0200

    Stable Engine

--- class/engine.php
+++ class/engine.php
@@ -161,34 +161,4 @@ class Engine extends MySQL {
 	}
 }
 
-function show_forums () {
-	$header = new Engine("header.tp");
-	$index = new Engine("index.tp");
-	$cat = new Engine("cat.tp");
-	$forum = new Engine("forum.tp");
-	$footer = new Engine("footer.tp");
-	
-	$header->fill('title','Home');
-	
-	echo $header->render();
-	echo $index->render();
-	
-	
-	$conn = new MySQL();
-	$res_cat = $conn->query("SELECT * FROM categories");
-	
-	while ($row = mysql_fetch_assoc($res_cat)) {
-		$id = $row['id'];
-		$cat = new Engine("cat.tp");
-		$cat->autofill("SELECT name FROM categories WHERE id = '$id'");
-		$sect = new Engine("forum.tp");
-		$sect->autofill_iter("SELECT forum_name FROM forums WHERE category = '$id'");
-		$cat->append($sect);
-		echo $cat->full_render();
-	}
-	
-	echo $footer->render();
-
-}
-
 ?>
