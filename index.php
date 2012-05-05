1cedbc231bda32fa30b2588ecf2de2cdaf5fc5bf
SystemOveride <system_overide@live.i>
2012-05-05 13:14:56 +0200

    Index example

--- index.php
+++ index.php
@@ -2,20 +2,10 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-require_once 'class/template.php';
+include("class/engine.php");
 include_once("class/user.php");
-include_once("class/forums.php");
+include_once("class/db.php");
 
-$template = new Template();
-
-$template->head("Home");
-$template->load("login.tp");
-$template->load("signup.tp");
-
-if(User::getInstance()->is_logged()) {
-	echo "Logged !";
-}
-
-Forum::getInstance()->show_forums();
+show_forums();
 
 ?>
